a649ae9f775a4ce4fb48981878d63583727afa0e
kyonshi0104 <155651185+kyonshi0104@users.noreply.github.com>
2024-01-14 22:21:48 +0000

    kyonshi_bot.main
    49

--- main.py
+++ main.py
@@ -543,6 +543,9 @@ async def on_message(message):
     elif any(mention in message.content for mention in mentions):
       await message.channel.send("everyoneメンションまたはhereメンションを検知しました。 ")
       return
+    elif any(link in message.content for link in links):
+      await message.channel.send("招待リンクを検知しました。 ")
+      return
     response = response.replace("@", "＠")
     await message.channel.send(response)
 
