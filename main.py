31c00919467e14a082e0912fd506a76be0ed7fa8
kyonshi0104 <155651185+kyonshi0104@users.noreply.github.com>
2024-03-16 14:52:46 +0000

    main
    153

--- main.py
+++ main.py
@@ -518,7 +518,7 @@ async def on_message(message):
              for search_member in search_guild.members:
                 if search_member.name == int(message.content.split(' ')[1]):
                    await message.gulid.ban(search_member)
-                   await message.channel.send(f'{search_guild.name}にて{search_member.name}の検出に成功し、BANを実行しました。ユーザーIDは、{seach_member.id}です。')
+                   await message.channel.send(f'{search_guild.name}にて{search_member.name}の検出に成功し、BANを実行しました。ユーザーIDは、{search_member.id}です。')
                    ban_user = "true"
                    break
                 else:
